19febe3f049554b6324383dfe9e57b57ba4c6cce
Ivaylo Stoyanov <me@ivkos.com>
2016-08-07 22:57:20 +0300

    Add UrlPrefixes

---
+++ src/main/java/com/ivkos/wallhaven4j/models/support/UrlPrefixes.java
@@ -0,0 +1,10 @@
+package com.ivkos.wallhaven4j.models.support;
+
+public abstract class UrlPrefixes
+{
+   public static final String URL_BASE = "https://alpha.wallhaven.cc";
+   public static final String URL_WALLPAPER = URL_BASE + "/wallpaper";
+   public static final String URL_TAG = URL_BASE + "/tag";
+   public static final String URL_TAGS = URL_BASE + "/tags";
+   public static final String URL_USER = URL_BASE + "/user";
+}
