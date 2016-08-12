eb9a12f6228577a9249646ee8c3d3354cd24e17f
Ivaylo Stoyanov <me@ivkos.com>
2016-08-12 14:17:38 +0300

    Add exceptions

---
+++ src/main/java/com/ivkos/wallhaven4j/support/exceptions/ParseException.java
@@ -0,0 +1,28 @@
+package com.ivkos.wallhaven4j.support.exceptions;
+
+public class ParseException extends RuntimeException
+{
+   public ParseException()
+   {
+   }
+
+   public ParseException(String message)
+   {
+      super(message);
+   }
+
+   public ParseException(String message, Throwable cause)
+   {
+      super(message, cause);
+   }
+
+   public ParseException(Throwable cause)
+   {
+      super(cause);
+   }
+
+   public ParseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+   {
+      super(message, cause, enableSuppression, writableStackTrace);
+   }
+}
---
+++ src/main/java/com/ivkos/wallhaven4j/support/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,28 @@
+package com.ivkos.wallhaven4j.support.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException
+{
+   public ResourceNotFoundException()
+   {
+   }
+
+   public ResourceNotFoundException(String message)
+   {
+      super(message);
+   }
+
+   public ResourceNotFoundException(String message, Throwable cause)
+   {
+      super(message, cause);
+   }
+
+   public ResourceNotFoundException(Throwable cause)
+   {
+      super(cause);
+   }
+
+   public ResourceNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+   {
+      super(message, cause, enableSuppression, writableStackTrace);
+   }
+}
