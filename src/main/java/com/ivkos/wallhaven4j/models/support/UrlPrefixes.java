bf4ec23ef02c3881abe2469c80083f469eecac3f
Ivaylo Stoyanov <me@ivkos.com>
2016-08-13 17:36:32 +0300

    Make UrlPrefixes final class with private ctor

--- src/main/java/com/ivkos/wallhaven4j/models/support/UrlPrefixes.java
+++ src/main/java/com/ivkos/wallhaven4j/models/support/UrlPrefixes.java
@@ -1,10 +1,14 @@
 package com.ivkos.wallhaven4j.models.support;
 
-public abstract class UrlPrefixes
+public final class UrlPrefixes
 {
    public static final String URL_BASE = "https://alpha.wallhaven.cc";
    public static final String URL_WALLPAPER = URL_BASE + "/wallpaper";
    public static final String URL_TAG = URL_BASE + "/tag";
    public static final String URL_TAGS = URL_BASE + "/tags";
    public static final String URL_USER = URL_BASE + "/user";
+
+   private UrlPrefixes()
+   {
+   }
 }
