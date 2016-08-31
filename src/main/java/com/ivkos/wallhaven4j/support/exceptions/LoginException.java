1773003425c7e2870b0ca32a39754d9ff3c9998c
Ivaylo Stoyanov <me@ivkos.com>
2016-08-31 22:19:22 +0300

    Add LoginException

---
+++ src/main/java/com/ivkos/wallhaven4j/support/exceptions/LoginException.java
@@ -0,0 +1,28 @@
+package com.ivkos.wallhaven4j.support.exceptions;
+
+public class LoginException extends RuntimeException
+{
+   public LoginException()
+   {
+   }
+
+   public LoginException(String message)
+   {
+      super(message);
+   }
+
+   public LoginException(String message, Throwable cause)
+   {
+      super(message, cause);
+   }
+
+   public LoginException(Throwable cause)
+   {
+      super(cause);
+   }
+
+   public LoginException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+   {
+      super(message, cause, enableSuppression, writableStackTrace);
+   }
+}
