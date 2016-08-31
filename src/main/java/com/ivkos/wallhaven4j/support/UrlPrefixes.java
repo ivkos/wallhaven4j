f4f148fd634a9302258ab18b4ba3f16870723140
Ivaylo Stoyanov <me@ivkos.com>
2016-08-31 22:20:48 +0300

    Add login URL

--- src/main/java/com/ivkos/wallhaven4j/support/UrlPrefixes.java
+++ src/main/java/com/ivkos/wallhaven4j/support/UrlPrefixes.java
@@ -7,6 +7,7 @@ public final class UrlPrefixes
    public static final String URL_TAG = URL_BASE + "/tag";
    public static final String URL_TAGS = URL_BASE + "/tags";
    public static final String URL_USER = URL_BASE + "/user";
+   public static final String URL_LOGIN = URL_BASE + "/auth/login";
 
    private UrlPrefixes()
    {
