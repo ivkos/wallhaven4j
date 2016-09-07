4a7ff8ab89efbb101687eaee70bb5b511e762f77
Ivaylo Stoyanov <me@ivkos.com>
2016-09-07 14:05:23 +0300

    Update enums Category & Purity to contain numeric values

--- src/main/java/com/ivkos/wallhaven4j/models/misc/enums/Category.java
+++ src/main/java/com/ivkos/wallhaven4j/models/misc/enums/Category.java
@@ -2,5 +2,17 @@ package com.ivkos.wallhaven4j.models.misc.enums;
 
 public enum Category
 {
-   GENERAL, ANIME, PEOPLE
+   GENERAL(4), ANIME(2), PEOPLE(1);
+
+   private final long value;
+
+   Category(long value)
+   {
+      this.value = value;
+   }
+
+   public long getValue()
+   {
+      return value;
+   }
 }
--- src/main/java/com/ivkos/wallhaven4j/models/misc/enums/Purity.java
+++ src/main/java/com/ivkos/wallhaven4j/models/misc/enums/Purity.java
@@ -2,5 +2,17 @@ package com.ivkos.wallhaven4j.models.misc.enums;
 
 public enum Purity
 {
-   SFW, SKETCHY, NSFW
+   SFW(4), SKETCHY(2), NSFW(1);
+
+   private final long value;
+
+   Purity(long value)
+   {
+      this.value = value;
+   }
+
+   public long getValue()
+   {
+      return value;
+   }
 }
