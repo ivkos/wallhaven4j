a108a74b209a4f3d1563095514f102322cf341be
Ivaylo Stoyanov <me@ivkos.com>
2016-09-09 23:35:32 +0300

    Add TimeElementParser

---
+++ src/main/java/com/ivkos/wallhaven4j/util/htmlparser/TimeElementParser.java
@@ -0,0 +1,29 @@
+package com.ivkos.wallhaven4j.util.htmlparser;
+
+import com.ivkos.wallhaven4j.util.exceptions.ParseException;
+import org.joda.time.DateTime;
+
+public class TimeElementParser
+{
+   private TimeElementParser() {}
+
+   public static DateTime parse(HtmlElement htmlElement)
+   {
+      if (!htmlElement.getTagName().equals("time")) {
+         throw new IllegalArgumentException("htmlElement must be a 'time' element");
+      }
+
+      String datetime = htmlElement.getAttribute("datetime");
+
+      if (datetime.isEmpty()) throw new ParseException("datetime is empty");
+
+      DateTime result;
+      try {
+         result = DateTime.parse(datetime);
+      } catch (IllegalArgumentException e) {
+         throw new ParseException("Could not parse time element", e);
+      }
+
+      return result;
+   }
+}
