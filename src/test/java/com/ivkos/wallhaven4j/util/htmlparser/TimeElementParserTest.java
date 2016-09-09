591d1b0fed1c653e0940a870c15ec6da4c9ae125
Ivaylo Stoyanov <me@ivkos.com>
2016-09-10 00:59:11 +0300

    Add TimeElementParserTest

---
+++ src/test/java/com/ivkos/wallhaven4j/util/htmlparser/TimeElementParserTest.java
@@ -0,0 +1,217 @@
+package com.ivkos.wallhaven4j.util.htmlparser;
+
+import com.ivkos.wallhaven4j.util.exceptions.ParseException;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class TimeElementParserTest
+{
+   @Test(expected = IllegalArgumentException.class)
+   public void parseWrongElement() throws Exception
+   {
+      HtmlElement div = new HtmlElement()
+      {
+         @Override
+         public List<HtmlElement> find(String cssQuery)
+         {
+            return null;
+         }
+
+         @Override
+         public HtmlElement findFirst(String cssQuery)
+         {
+            return null;
+         }
+
+         @Override
+         public HtmlElement findLast(String cssQuery)
+         {
+            return null;
+         }
+
+         @Override
+         public HtmlElement findElementById(String id)
+         {
+            return null;
+         }
+
+         @Override
+         public String getText()
+         {
+            return null;
+         }
+
+         @Override
+         public String getAttribute(String attributeName)
+         {
+            return null;
+         }
+
+         @Override
+         public boolean hasClass(String className)
+         {
+            return false;
+         }
+
+         @Override
+         public Map<String, String> getDataAttributes()
+         {
+            return null;
+         }
+
+         @Override
+         public String getValue()
+         {
+            return null;
+         }
+
+         @Override
+         public String getTagName()
+         {
+            return "div";
+         }
+      };
+
+      TimeElementParser.parse(div);
+   }
+
+   @Test(expected = ParseException.class)
+   public void parseEmptyDateTime() throws Exception
+   {
+      HtmlElement div = new HtmlElement()
+      {
+         @Override
+         public List<HtmlElement> find(String cssQuery)
+         {
+            return null;
+         }
+
+         @Override
+         public HtmlElement findFirst(String cssQuery)
+         {
+            return null;
+         }
+
+         @Override
+         public HtmlElement findLast(String cssQuery)
+         {
+            return null;
+         }
+
+         @Override
+         public HtmlElement findElementById(String id)
+         {
+            return null;
+         }
+
+         @Override
+         public String getText()
+         {
+            return null;
+         }
+
+         @Override
+         public String getAttribute(String attributeName)
+         {
+            return "";
+         }
+
+         @Override
+         public boolean hasClass(String className)
+         {
+            return false;
+         }
+
+         @Override
+         public Map<String, String> getDataAttributes()
+         {
+            return null;
+         }
+
+         @Override
+         public String getValue()
+         {
+            return null;
+         }
+
+         @Override
+         public String getTagName()
+         {
+            return "time";
+         }
+      };
+
+      TimeElementParser.parse(div);
+   }
+
+   @Test(expected = ParseException.class)
+   public void parseIllegalDatetime() throws Exception
+   {
+      HtmlElement div = new HtmlElement()
+      {
+         @Override
+         public List<HtmlElement> find(String cssQuery)
+         {
+            return null;
+         }
+
+         @Override
+         public HtmlElement findFirst(String cssQuery)
+         {
+            return null;
+         }
+
+         @Override
+         public HtmlElement findLast(String cssQuery)
+         {
+            return null;
+         }
+
+         @Override
+         public HtmlElement findElementById(String id)
+         {
+            return null;
+         }
+
+         @Override
+         public String getText()
+         {
+            return null;
+         }
+
+         @Override
+         public String getAttribute(String attributeName)
+         {
+            return "a1b2c3";
+         }
+
+         @Override
+         public boolean hasClass(String className)
+         {
+            return false;
+         }
+
+         @Override
+         public Map<String, String> getDataAttributes()
+         {
+            return null;
+         }
+
+         @Override
+         public String getValue()
+         {
+            return null;
+         }
+
+         @Override
+         public String getTagName()
+         {
+            return "time";
+         }
+      };
+
+      TimeElementParser.parse(div);
+   }
+}
