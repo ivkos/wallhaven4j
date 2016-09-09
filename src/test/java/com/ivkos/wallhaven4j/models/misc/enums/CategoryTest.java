dd2702ab9be517023ffe06aec7634705fe891e58
Ivaylo Stoyanov <me@ivkos.com>
2016-09-10 01:30:20 +0300

    Category & Purity: Add more tests

--- src/test/java/com/ivkos/wallhaven4j/models/misc/enums/CategoryTest.java
+++ src/test/java/com/ivkos/wallhaven4j/models/misc/enums/CategoryTest.java
@@ -1,17 +1,26 @@
 package com.ivkos.wallhaven4j.models.misc.enums;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static com.ivkos.wallhaven4j.models.misc.enums.Category.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class CategoryTest
 {
    @Test
    public void valuesAreCorrect() throws Exception
    {
-      Assert.assertEquals(4, GENERAL.getBitfieldValue());
-      Assert.assertEquals(2, ANIME.getBitfieldValue());
-      Assert.assertEquals(1, PEOPLE.getBitfieldValue());
+      assertEquals(4, GENERAL.getBitfieldValue());
+      assertEquals(2, ANIME.getBitfieldValue());
+      assertEquals(1, PEOPLE.getBitfieldValue());
+   }
+
+   @Test
+   public void constantsArePresent() throws Exception
+   {
+      assertNotNull(Category.valueOf("GENERAL"));
+      assertNotNull(Category.valueOf("ANIME"));
+      assertNotNull(Category.valueOf("PEOPLE"));
    }
 }
--- src/test/java/com/ivkos/wallhaven4j/models/misc/enums/PurityTest.java
+++ src/test/java/com/ivkos/wallhaven4j/models/misc/enums/PurityTest.java
@@ -1,17 +1,26 @@
 package com.ivkos.wallhaven4j.models.misc.enums;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static com.ivkos.wallhaven4j.models.misc.enums.Purity.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class PurityTest
 {
    @Test
    public void valuesAreCorrect() throws Exception
    {
-      Assert.assertEquals(4, SFW.getBitfieldValue());
-      Assert.assertEquals(2, SKETCHY.getBitfieldValue());
-      Assert.assertEquals(1, NSFW.getBitfieldValue());
+      assertEquals(4, SFW.getBitfieldValue());
+      assertEquals(2, SKETCHY.getBitfieldValue());
+      assertEquals(1, NSFW.getBitfieldValue());
+   }
+
+   @Test
+   public void constantsArePresent() throws Exception
+   {
+      assertNotNull(Purity.valueOf("SFW"));
+      assertNotNull(Purity.valueOf("SKETCHY"));
+      assertNotNull(Purity.valueOf("NSFW"));
    }
 }
