253efb0e6286334631efbc0fcb791e11d478fbb6
Ivaylo Stoyanov <me@ivkos.com>
2016-09-10 18:58:08 +0300

    Add JsonSerializer.fromJson(String, Type)

--- src/main/java/com/ivkos/wallhaven4j/util/jsonserializer/GsonJsonSerializer.java
+++ src/main/java/com/ivkos/wallhaven4j/util/jsonserializer/GsonJsonSerializer.java
@@ -3,6 +3,8 @@ package com.ivkos.wallhaven4j.util.jsonserializer;
 import com.google.gson.Gson;
 import com.google.inject.Inject;
 
+import java.lang.reflect.Type;
+
 public class GsonJsonSerializer implements JsonSerializer
 {
    private final Gson gson;
@@ -23,5 +25,11 @@ public class GsonJsonSerializer implements JsonSerializer
    public <T> T fromJson(String json, Class<T> type)
    {
       return gson.fromJson(json, type);
+   }
+
+   @Override
+   public <T> T fromJson(String json, Type typeOfT)
+   {
+      return gson.fromJson(json, typeOfT);
    }
 }
--- src/main/java/com/ivkos/wallhaven4j/util/jsonserializer/JsonSerializer.java
+++ src/main/java/com/ivkos/wallhaven4j/util/jsonserializer/JsonSerializer.java
@@ -1,8 +1,12 @@
 package com.ivkos.wallhaven4j.util.jsonserializer;
 
+import java.lang.reflect.Type;
+
 public interface JsonSerializer
 {
    String toJson(Object src);
 
    <T> T fromJson(String json, Class<T> type);
+
+   <T> T fromJson(String json, Type typeOfT);
 }
