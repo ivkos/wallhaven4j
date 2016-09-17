7d25e3544857c222fbe9c4f53afee03e8a28dc75
Ivaylo Stoyanov <me@ivkos.com>
2016-09-17 12:50:20 +0300

    Add javadocs for SearchQueryBuilder

--- src/main/java/com/ivkos/wallhaven4j/util/searchquery/SearchQueryBuilder.java
+++ src/main/java/com/ivkos/wallhaven4j/util/searchquery/SearchQueryBuilder.java
@@ -31,6 +31,15 @@ public class SearchQueryBuilder
       return Preconditions.checkNotNull(ref, "%s must not be null", refName);
    }
 
+   //region keywords
+
+   /**
+    * Sets keywords to search for.
+    *
+    * @param keywords A collection of keywords
+    * @return the current <code>SearchQueryBuilder</code>
+    * @throws NullPointerException if the collection of keywords is <code>null</code>
+    */
    public SearchQueryBuilder keywords(Collection<String> keywords)
    {
       checkNotNull(keywords, "keywords");
@@ -45,11 +54,28 @@ public class SearchQueryBuilder
       return this;
    }
 
+   /**
+    * Sets keywords to search for.
+    *
+    * @param keywords An array or varargs of keywords
+    * @return the current <code>SearchQueryBuilder</code>
+    */
    public SearchQueryBuilder keywords(String... keywords)
    {
       return keywords(asList(keywords));
    }
+   //endregion
 
+   //region categories
+
+   /**
+    * Sets the categories of wallpapers to include in search result.
+    *
+    * @param categories A collection of categories
+    * @return the current <code>SearchQueryBuilder</code>
+    * @throws NullPointerException if the collection of categories is <code>null</code>
+    * @see Category
+    */
    public SearchQueryBuilder categories(Collection<Category> categories)
    {
       checkNotNull(categories, "categories");
@@ -63,11 +89,29 @@ public class SearchQueryBuilder
       return this;
    }
 
+   /**
+    * Sets the categories of wallpapers to include in search result.
+    *
+    * @param categories An array or varargs of categories
+    * @return the current <code>SearchQueryBuilder</code>
+    * @see Category
+    */
    public SearchQueryBuilder categories(Category... categories)
    {
       return categories(ImmutableSet.copyOf(categories));
    }
+   //endregion
 
+   //region purity
+
+   /**
+    * Set the purity of wallpapers to include in search result.
+    *
+    * @param purity Collection of purities.
+    * @return the current <code>SearchQueryBuilder</code>
+    * @throws NullPointerException if the collection of purities is <code>null</code>
+    * @see Purity
+    */
    public SearchQueryBuilder purity(Collection<Purity> purity)
    {
       checkNotNull(purity, "purity");
@@ -81,11 +125,27 @@ public class SearchQueryBuilder
       return this;
    }
 
+   /**
+    * Set the purity of wallpapers to include in search result.
+    *
+    * @param purity An array or varargs of purities.
+    * @return the current <code>SearchQueryBuilder</code>
+    * @see Purity
+    */
    public SearchQueryBuilder purity(Purity... purity)
    {
       return purity(ImmutableSet.copyOf(purity));
    }
+   //endregion
 
+   /**
+    * Sets the property that wallpapers in search result will be sorted by.
+    *
+    * @param sorting Sorting property.
+    * @return the current <code>SearchQueryBuilder</code>
+    * @throws NullPointerException if sorting property <code>null</code>
+    * @see Sorting
+    */
    public SearchQueryBuilder sorting(Sorting sorting)
    {
       checkNotNull(sorting, "sorting");
@@ -94,6 +154,14 @@ public class SearchQueryBuilder
       return this;
    }
 
+   /**
+    * Sets the order that wallpapers in search result will be returned.
+    *
+    * @param order Order. Could be either <code>Order.DESC</code>, or <code>Order.ASC</code>.
+    * @return the current <code>SearchQueryBuilder</code>
+    * @throws NullPointerException if sorting property <code>null</code>
+    * @see Order
+    */
    public SearchQueryBuilder order(Order order)
    {
       checkNotNull(order, "order");
@@ -102,6 +170,16 @@ public class SearchQueryBuilder
       return this;
    }
 
+   //region resolutions
+
+   /**
+    * Sets the resolutions of wallpapers to include in search result.
+    *
+    * @param resolutions A collection of <code>Resolution</code>s
+    * @return the current <code>SearchQueryBuilder</code>
+    * @throws NullPointerException if the collection of <code>Resolution</code>s is <code>null</code>
+    * @see Resolution
+    */
    public SearchQueryBuilder resolutions(Collection<Resolution> resolutions)
    {
       checkNotNull(resolutions, "resolutions");
@@ -115,11 +193,29 @@ public class SearchQueryBuilder
       return this;
    }
 
+   /**
+    * Sets the resolutions of wallpapers to include in search result.
+    *
+    * @param resolutions An array or varargs of <code>Resolution</code>s
+    * @return the current <code>SearchQueryBuilder</code>
+    * @see Resolution
+    */
    public SearchQueryBuilder resolutions(Resolution... resolutions)
    {
       return resolutions(ImmutableSet.copyOf(resolutions));
    }
+   //endregion
 
+   //region ratios
+
+   /**
+    * Sets the ratios of wallpapers to include in search result.
+    *
+    * @param ratios A collection of <code>Ratio</code>s
+    * @return the current <code>SearchQueryBuilder</code>
+    * @throws NullPointerException if the collection of <code>Ratio</code>s is <code>null</code>
+    * @see Ratio
+    */
    public SearchQueryBuilder ratios(Collection<Ratio> ratios)
    {
       checkNotNull(ratios, "ratios");
@@ -133,11 +229,28 @@ public class SearchQueryBuilder
       return this;
    }
 
+   /**
+    * Sets the ratios of wallpapers to include in search result.
+    *
+    * @param ratios A collection of <code>Ratio</code>s
+    * @return the current <code>SearchQueryBuilder</code>
+    * @see Ratio
+    */
    public SearchQueryBuilder ratios(Ratio... ratios)
    {
       return ratios(ImmutableSet.copyOf(ratios));
    }
+   //endregion
 
+   /**
+    * Sets how many pages of wallpapers to fetch.
+    * One page could contain 24 (default), 32 or 64 wallpapers,
+    * depending on the preference of the logged in user, set in Wallhaven's settings page.
+    *
+    * @param pages The number of pages to fetch.
+    * @return the current <code>SearchQueryBuilder</code>
+    * @throws IllegalArgumentException if <code>pages</code> is not greater than zero
+    */
    public SearchQueryBuilder pages(long pages)
    {
       Preconditions.checkArgument(pages > 0, "pages must be greater than zero");
@@ -146,6 +259,11 @@ public class SearchQueryBuilder
       return this;
    }
 
+   /**
+    * Builds a <code>SearchQuery</code> object.
+    *
+    * @return a <code>SearchQuery</code> object.
+    */
    public SearchQuery build()
    {
       return query;
