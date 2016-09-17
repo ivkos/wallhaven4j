7a5a7a08705e996a7f76a728f2201ffa23b652a0
Ivaylo Stoyanov <me@ivkos.com>
2016-09-17 14:16:07 +0300

    Add Wallhaven negative tests

--- src/test/java/com/ivkos/wallhaven4j/WallhavenTest.java
+++ src/test/java/com/ivkos/wallhaven4j/WallhavenTest.java
@@ -1,5 +1,6 @@
 package com.ivkos.wallhaven4j;
 
+import com.ivkos.wallhaven4j.models.user.User;
 import com.ivkos.wallhaven4j.util.exceptions.LoginException;
 import com.ivkos.wallhaven4j.util.exceptions.ResourceNotAccessibleException;
 import com.ivkos.wallhaven4j.util.exceptions.ResourceNotFoundException;
@@ -27,6 +28,76 @@ public class WallhavenTest extends AbstractWallhavenTest
    public void getNsfwWallpaperWhenNotLoggedIn() throws Exception
    {
       getWallhaven().getWallpaper(8273);
+   }
+
+   @Test
+   public void getCurrentUser() throws Exception
+   {
+      assertNull(getWallhaven().getCurrentUser());
+
+      User currentUser = getWallhaven(true).getCurrentUser();
+      assertNotNull(currentUser);
+      assertEquals(getWallhavenUsername(), currentUser.getUsername());
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void getTagWithZeroId() throws Exception
+   {
+      getWallhaven().getTag(0);
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void getTagWithNegativeId() throws Exception
+   {
+      getWallhaven().getTag(-1);
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void getUserWithNullUsername() throws Exception
+   {
+      getWallhaven().getUser(null);
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void getUserWithEmptyUsername() throws Exception
+   {
+      getWallhaven().getUser("");
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void getWallpaperWithZeroId() throws Exception
+   {
+      getWallhaven().getWallpaper(0);
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void getWallpaperWithNegativeId() throws Exception
+   {
+      getWallhaven().getWallpaper(-1);
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void getWallpaperCollectionWithNullUsername() throws Exception
+   {
+      getWallhaven().getWallpaperCollection(null, 0);
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void getWallpaperCollectionWithEmptyUsername() throws Exception
+   {
+      getWallhaven().getWallpaperCollection("", 0);
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void getWallpaperCollectionWithZeroId() throws Exception
+   {
+      getWallhaven().getWallpaperCollection("someone", 0);
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void getWallpaperCollectionWithNegativeId() throws Exception
+   {
+      getWallhaven().getWallpaperCollection("someone", -1);
    }
 
    @Test
