541fa7937b42a08adaffde008a722232ada32a87
Ivaylo Stoyanov <me@ivkos.com>
2016-09-17 13:55:26 +0300

    Add argument checks and javadocs to Wallhaven class

--- src/main/java/com/ivkos/wallhaven4j/Wallhaven.java
+++ src/main/java/com/ivkos/wallhaven4j/Wallhaven.java
@@ -1,17 +1,22 @@
 package com.ivkos.wallhaven4j;
 
-import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.ivkos.wallhaven4j.models.ResourceFactoryFactory;
 import com.ivkos.wallhaven4j.models.tag.Tag;
 import com.ivkos.wallhaven4j.models.user.User;
 import com.ivkos.wallhaven4j.models.wallpaper.Wallpaper;
+import com.ivkos.wallhaven4j.models.wallpapercollection.WallpaperCollection;
+import com.ivkos.wallhaven4j.models.wallpapercollection.WallpaperCollectionIdentifier;
 import com.ivkos.wallhaven4j.util.WallhavenGuiceModule;
 import com.ivkos.wallhaven4j.util.WallhavenSession;
+import com.ivkos.wallhaven4j.util.exceptions.LoginException;
 import com.ivkos.wallhaven4j.util.exceptions.ResourceNotFoundException;
 
 import java.io.File;
+
+import static com.google.common.base.Preconditions.checkArgument;
 
 public class Wallhaven
 {
@@ -19,6 +24,11 @@ public class Wallhaven
    private final WallhavenSession session;
    private final ResourceFactoryFactory rff;
 
+   /**
+    * Creates a new anonymous Wallhaven session.
+    *
+    * @param cookiesFile file to save cookies in
+    */
    public Wallhaven(File cookiesFile)
    {
       injector = Guice.createInjector(new WallhavenGuiceModule(cookiesFile));
@@ -26,6 +36,14 @@ public class Wallhaven
       rff = injector.getInstance(ResourceFactoryFactory.class);
    }
 
+   /**
+    * Creates a new Wallhaven session and logs in with the specified credentials.
+    *
+    * @param username    the username
+    * @param password    the password
+    * @param cookiesFile file to save cookies in
+    * @throws LoginException if the credentials are incorrect
+    */
    public Wallhaven(String username, String password, File cookiesFile)
    {
       injector = Guice.createInjector(new WallhavenGuiceModule(cookiesFile));
@@ -35,40 +53,98 @@ public class Wallhaven
       session.login(username, password);
    }
 
+   /**
+    * Creates a new anonymous Wallhaven session.
+    */
    public Wallhaven()
    {
       this(null);
    }
 
+   /**
+    * Creates a new Wallhaven session and logs in with the specified credentials.
+    *
+    * @param username the username
+    * @param password the password
+    * @throws LoginException if the credentials are incorrect
+    */
    public Wallhaven(String username, String password)
    {
       this(username, password, null);
    }
 
+   /**
+    * Returns the currently logged in user, or <code>null</code> if the session is anonymous.
+    *
+    * @return the currently logged in user, or <code>null</code> if the session is anonymous.
+    */
    public User getCurrentUser()
    {
       return session.getCurrentUser();
    }
 
    /**
-    * @param id Wallpaper's id
+    * Finds the tag with the specified id and returns a {@link Tag} object that represents it.
+    *
+    * @param id the tag's id
+    * @return the tag
+    * @throws ResourceNotFoundException if there is no tag with the specified id
+    * @throws IllegalArgumentException  if the specified id is not greater than zero
+    */
+   public Tag getTag(long id)
+   {
+      checkArgument(id > 0);
+
+      return rff.getFactoryFor(Tag.class).create(true, id);
+   }
+
+   /**
+    * Finds the user with the specified username and returns a {@link User} object that represents it.
+    *
+    * @param username the username
+    * @return the tag
+    * @throws ResourceNotFoundException if there is no user with the specified username
+    * @throws IllegalArgumentException  if the specified username is <code>null</code> or empty
+    */
+   public User getUser(String username)
+   {
+      checkArgument(!Strings.isNullOrEmpty(username), "username must not be null or empty");
+
+      return rff.getFactoryFor(User.class).create(true, username);
+   }
+
+   /**
+    * Finds the wallpaper with the specified id and returns a {@link Wallpaper} object that represents it.
+    *
+    * @param id the wallpaper's id
     * @return the wallpaper
-    * @throws ResourceNotFoundException if there's wallpaper with the set id
+    * @throws ResourceNotFoundException if there is no wallpaper with the specified id
+    * @throws IllegalArgumentException  if the specified id is not greater than zero
     */
    public Wallpaper getWallpaper(long id)
    {
-      Preconditions.checkArgument(id > 0);
+      checkArgument(id > 0);
 
       return rff.getFactoryFor(Wallpaper.class).create(true, id);
    }
 
-   public Tag getTag(long id)
+   /**
+    * Finds the wallpaper collection with the specified id owned by the specified user
+    * and returns a {@link WallpaperCollection} object that represents it.
+    *
+    * @param username the username of the user that owns the wallpaper collection
+    * @param id       the wallpaper collection id
+    * @return the wallpaper collection
+    * @throws ResourceNotFoundException if there no such wallpaper collection
+    * @throws IllegalArgumentException  if the username is <code>null</code> or empty, or the id is not greater than zero
+    */
+   public WallpaperCollection getWallpaperCollection(String username, long id)
    {
-      return rff.getFactoryFor(Tag.class).create(true, id);
-   }
+      checkArgument(!Strings.isNullOrEmpty(username), "username must not be null or empty");
+      checkArgument(id > 0);
 
-   public User getUser(String username)
-   {
-      return rff.getFactoryFor(User.class).create(true, username);
+      User user = rff.getFactoryFor(User.class).create(false, username);
+
+      return rff.getFactoryFor(WallpaperCollection.class).create(true, new WallpaperCollectionIdentifier(id, user));
    }
 }
