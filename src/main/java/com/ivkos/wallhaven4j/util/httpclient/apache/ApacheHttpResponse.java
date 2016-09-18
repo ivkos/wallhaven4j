c8d9a48ca0183760f9a9dc88a5e798976a519004
Ivaylo Stoyanov <me@ivkos.com>
2016-09-18 12:43:12 +0300

    ApacheHttpResponse: Preload body eagerly and close stream to prevent pool exhaustion

--- src/main/java/com/ivkos/wallhaven4j/util/httpclient/apache/ApacheHttpResponse.java
+++ src/main/java/com/ivkos/wallhaven4j/util/httpclient/apache/ApacheHttpResponse.java
@@ -3,6 +3,7 @@ package com.ivkos.wallhaven4j.util.httpclient.apache;
 import com.ivkos.wallhaven4j.util.exceptions.ConnectionException;
 import com.ivkos.wallhaven4j.util.httpclient.HttpResponse;
 import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
@@ -14,10 +15,12 @@ import static java.util.Collections.unmodifiableMap;
 class ApacheHttpResponse implements HttpResponse
 {
    private final org.apache.http.HttpResponse apacheResponse;
+   private final String body;
 
    ApacheHttpResponse(org.apache.http.HttpResponse apacheResponse)
    {
       this.apacheResponse = apacheResponse;
+      this.body = getBodyEagerly();
    }
 
    @Override
@@ -29,11 +32,22 @@ class ApacheHttpResponse implements HttpResponse
    @Override
    public String getBody()
    {
+      return body;
+   }
+
+   private String getBodyEagerly()
+   {
+      HttpEntity theEntity = apacheResponse.getEntity();
+
+      String body;
       try {
-         return EntityUtils.toString(apacheResponse.getEntity());
+         body = EntityUtils.toString(theEntity);
+         EntityUtils.consume(theEntity);
       } catch (IOException e) {
-         throw new ConnectionException("Could not get response entity", e);
+         throw new ConnectionException("Could not get response body", e);
       }
+
+      return body;
    }
 
    @Override
