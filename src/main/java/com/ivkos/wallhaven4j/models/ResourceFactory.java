da37ea76946178d3e24ac449e9459761f920773b
Ivaylo Stoyanov <me@ivkos.com>
2016-10-06 19:00:54 +0300

    ResourceFactory: Add type parameter bound

--- src/main/java/com/ivkos/wallhaven4j/models/ResourceFactory.java
+++ src/main/java/com/ivkos/wallhaven4j/models/ResourceFactory.java
@@ -1,6 +1,6 @@
 package com.ivkos.wallhaven4j.models;
 
-public interface ResourceFactory<R extends AbstractResource, I>
+public interface ResourceFactory<R extends AbstractResource<I>, I>
 {
    R create(boolean preloadDom, I id);
 }
